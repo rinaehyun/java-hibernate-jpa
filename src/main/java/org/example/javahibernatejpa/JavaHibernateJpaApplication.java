12ae5034eb70fae71c18d6585e5690ee5cb42521
Rinae Hyun <rnhyun.dream@gmail.com>
2024-08-01 15:34:00 +0200

    Initial commit

---
+++ src/main/java/org/example/javahibernatejpa/JavaHibernateJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.javahibernatejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHibernateJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaHibernateJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javahibernatejpa/JavaHibernateJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javahibernatejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHibernateJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
